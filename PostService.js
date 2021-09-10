f0a60940b8f885b2158f57439df81379fae1728a
Eduard <moriartykayot678@mail.ru>
2021-09-11 01:08:14 +0300

    fix readme && fix model and service

--- PostService.js
+++ PostService.js
@@ -1,35 +1,35 @@
-import Post from "./Post.js";
+import PostModel from "./PostModel.js";
 import FileService from "./FileService.js";
 
 class PostService{
     async create(post, picture) {
         const fileName = FileService.saveFile(picture)
-        const createPost = await Post.create({...post, picture: fileName})
+        const createPost = await PostModel.create({...post, picture: fileName})
         return createPost;
     }
     async getOne(id) {
         if (!id) {
             throw new Error('not specified Id')
         }
-        const post = await Post.findById(id)
+        const post = await PostModel.findById(id)
         return post;
     }
     async getAll() {
-        const post = await Post.find()
+        const post = await PostModel.find()
         return post;
     }
     async update(post) {
         if (!post._id) {
             throw new Error('not specified Id')
         }
-        const newPost = await Post.findByIdAndUpdate(post._id, post, {new:true})
+        const newPost = await PostModel.findByIdAndUpdate(post._id, post, {new:true})
         return newPost;
     }
     async delete(id) {
         if (!id) {
             throw new Error('not specified Id')
         }
-        const post = await Post.findByIdAndDelete(id)
+        const post = await PostModel.findByIdAndDelete(id)
         return post;
     }
 }
